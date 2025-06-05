2fad49aa4670d9d476da548036a4c06458cc1bb8
Doctor-Dong <2362919128@qq.com>
2025-06-06 00:42:30 +0800

    codes for LeNet

---
+++ LeNet/class_function_reused.py
@@ -0,0 +1,43 @@
+import torch
+import time
+import numpy
+
+class Timer:
+    def __init__(self):
+        self.times=[]
+
+    def start(self):
+        self.start_time=time.time()
+
+    def stop(self): #记录并返回这段时长
+        self.times.append(time.time()-self.start_time)
+        return self.times[-1]
+    
+    def average(self):
+        return sum(self.times)/len(self.times)
+    
+    def total_time(self):
+        return sum(self.times)
+    
+    def cumsum_time(self):
+        return numpy.array(self.times).cumsum().tolist()
+
+class Accumulator:
+    def __init__(self,n):
+        self.data=[0.0]*n
+
+    def __getitem__(self,index):
+        return self.data[index]
+    
+    def add(self,*params):
+        self.data=[a+float(b) for a,b in zip(self.data,params) ]
+
+    def reset(self):
+        self.data=[0.0]*len(self.data)
+
+#define the number of predicting rightly
+def accuracy(y_hat,y):
+    if len(y_hat.shape)>1 and y_hat.shape[1]>1:
+        y_hat=y_hat.argmax(axis=1)
+    cmp=(y_hat.type(y.dtype)==y)
+    return float(cmp.type(y.dtype).sum())
